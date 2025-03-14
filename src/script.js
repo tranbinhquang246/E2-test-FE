d4f436b8c0756c9fe0e165d2d29b6e8b24720a17
Quang Tran <quangtran2462000@gmailcom>
2025-03-14 10:21:13 +0700

    Implement E2 practice UI

---
+++ src/script.js
@@ -0,0 +1,130 @@
+// Header Sticky script
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  const headers = document.querySelectorAll(".sticky-on-scroll");
+
+  headers.forEach((header) => {
+    if (currentScroll > lastScroll && currentScroll > 0) {
+      header.classList.add("hidden");
+    } else {
+      header.classList.remove("hidden");
+    }
+  });
+
+  if (currentScroll > 0) {
+    headers[1].classList.add("scrolled");
+  } else {
+    headers[1].classList.remove("scrolled");
+  }
+  lastScroll = currentScroll;
+});
+
+// Slider script
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  document.querySelector(".navbar").classList.toggle("active");
+});
+
+const indicators = document.querySelectorAll(".slider .indicators .indicator");
+const arrowPrev = document.querySelector(".slider .arrows .arrow-prev");
+const arrowNext = document.querySelector(".slider .arrows .arrow-next");
+const slides = document.querySelectorAll(".slider-inner .slide");
+const totalSlides = slides.length;
+
+let currentIndex = 0;
+let autoSlideInterval;
+
+const handleIndicatorClick = (event) => {
+  const indicator = event.target;
+  if (!isActive(indicator)) {
+    setActiveSlide(parseInt(indicator.dataset.slide) - 1);
+  }
+};
+
+indicators.forEach((indicator) => {
+  indicator.addEventListener("click", handleIndicatorClick);
+});
+
+function isActive(indicator) {
+  return indicator.hasAttribute("active");
+}
+
+function removeActive() {
+  document.querySelectorAll(".slider .indicators [active]").forEach((item) => {
+    item.removeAttribute("active");
+  });
+}
+
+function addActive(indicator) {
+  indicator.setAttribute("active", "");
+}
+
+function showSlide(index) {
+  const newPosition = (100 * index).toString();
+  document.querySelector(".slider-inner").style.marginLeft = `-${newPosition}%`;
+}
+
+function setActiveSlide(index) {
+  currentIndex = index;
+  removeActive();
+  addActive(indicators[index]);
+  showSlide(index);
+
+  slides.forEach((slide) =>
+    slide.querySelector(".content").classList.remove("active")
+  );
+
+  slides[index].querySelector(".content").classList.add("active");
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  setActiveSlide(currentIndex);
+}
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(nextSlide, 30000);
+}
+
+function stopAutoSlide() {
+  clearInterval(autoSlideInterval);
+}
+
+document.querySelector(".slider").addEventListener("mouseenter", stopAutoSlide);
+document
+  .querySelector(".slider")
+  .addEventListener("mouseleave", startAutoSlide);
+document.addEventListener("DOMContentLoaded", () => {
+  slides[currentIndex].querySelector(".content").classList.add("active");
+});
+
+startAutoSlide();
+
+// Tab script
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
+});
+
+document.querySelectorAll(".color-product").forEach((color) => {
+  color.addEventListener("click", function (event) {
+    event.preventDefault();
+    document
+      .querySelectorAll(".color-product")
+      .forEach((c) => c.classList.remove("selected"));
+    this.classList.add("selected");
+  });
+});
